b85f40388f679c6ffb4633c9a8717f5a4a5cdce6
sebas1195 <thesebas1195@gmail.com>
2021-03-13 17:19:59 +0100

    initial commit in new branch

---
+++ src/Levels/InvisWalls.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for tm in get_children():
+		tm.collision_layer = 9
+		tm.collision_mask = 9
+		tm.visible = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
